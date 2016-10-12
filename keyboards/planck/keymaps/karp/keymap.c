cf997ed93bad2074ee62620cc3b1c61a49cadc2d
Daniel Karp <danielkarp@gmail.com>
2016-10-12 14:07:24 -0400

    fixed braces

--- keyboards/planck/keymaps/karp/keymap.c
+++ keyboards/planck/keymaps/karp/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT) },
-  {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_LCBR), KC_SPC, KC_SPC, LT(_RAISE, KC_RCBR), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
+  {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_LBRC), KC_SPC, KC_SPC, LT(_RAISE, KC_RBRC), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
 },
 
 /* Colemak
