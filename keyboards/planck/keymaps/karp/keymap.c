9fa0f4d7a7056ee7b1bcc7c9c9e007951c3043c2
Daniel Karp <danielkarp@gmail.com>
2017-02-25 14:37:20 -0500

    add hyper key-

--- keyboards/planck/keymaps/karp/keymap.c
+++ keyboards/planck/keymaps/karp/keymap.c
@@ -51,10 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {ALL_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT) },
-  {BACKLIT, TG(_RAISE), KC_LGUI, KC_LALT,  KC_SPC, KC_EQL, LT(_LOWER, KC_LBRC), LT(_RAISE, KC_RBRC), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
+  {KC_HYPR, TG(_RAISE), KC_LGUI, KC_LALT,  KC_SPC, LT(_LOWER, KC_LBRC), LT(_RAISE, KC_RBRC), KC_EQL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
 },
 
 /* Colemak
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
+  {ALL_T(KC_TAB),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
   {CTL_T(KC_ESC), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT) },
-  {BACKLIT, TG(_RAISE), KC_LGUI, KC_LALT,  KC_SPC,  KC_EQL, LT(_LOWER, KC_LBRC), LT(_RAISE, KC_RBRC), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
+  {KC_HYPR, TG(_RAISE), KC_LGUI, KC_LALT,  KC_SPC, LT(_LOWER, KC_LBRC), LT(_RAISE, KC_RBRC), KC_EQL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
 },
 
 /* Dvorak
